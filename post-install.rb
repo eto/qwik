49b4ace9406740932428af3e17d100746f6dd44d
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2006-02-19 14:43:13 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@117 4820488e-9c09-0410-87d0-9d66fe36c344

--- post-install.rb
+++ post-install.rb
@@ -1,10 +1,29 @@
-%w(/var/cache/qwik /var/lib/qwik /var/log/qwik /var/run/qwik).each {|dir|
-  if ! File.exist?(dir)
-    system "mkdir #{dir}"
-  end
-  system "chmod 777 #{dir}"
-}
 system 'mkdir -p /var/lib/qwik/data/www'
 open('/var/lib/qwik/data/www/_SiteConfig.txt', 'wb') {|f|
   f.puts ':open:true'
 }
+open('/var/lib/qwik/data/www/_GroupMembers.txt', 'wb') {|f|
+  f.puts 'guest@qwik'
+}
+
+%w(
+/var/cache/qwik
+/var/lib/qwik
+/var/lib/qwik/data
+/var/lib/qwik/data/www
+/var/log/qwik
+/var/run/qwik
+).each {|dir|
+  system "mkdir #{dir}" if ! File.exist?(dir)
+  system "chown -R daemon.daemon #{dir}"
+  system "chmod -R go+w #{dir}"
+}
+
+=begin
+%w(
+/var/lib/qwik/data/www/_SiteConfig.txt
+/var/lib/qwik/data/www/_GroupMembers.txt
+).each {|file|
+  system "chmod -R go+w #{dir}"
+}
+=end
