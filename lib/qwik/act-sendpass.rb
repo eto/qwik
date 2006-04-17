e7263c42c247f1eb8427354c29e7ad0d7a27bc15
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2006-04-17 08:17:05 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@243 4820488e-9c09-0410-87d0-9d66fe36c344

---
+++ lib/qwik/act-sendpass.rb
@@ -0,0 +1,77 @@
+$LOAD_PATH << '..' unless $LOAD_PATH.include? '..'
+require 'qwik/act-getpass'
+require 'qwik/mailaddress'
+
+module Qwik
+  class Action
+    def act_sendpass
+      return sendpass_show_form if ! @req.is_post?
+
+      status = {}
+      members = @site.member.list
+      @req.query.each {|mail, v|
+	if ! MailAddress.valid?(mail) || v != 'on'
+	  status[mail] = _('Wrong format')
+	  next
+	end
+
+	if ! members.include?(mail)
+	  status[mail] = _('Not a member')
+	  next
+	end
+
+	passmail = generate_password_mail(mail)
+	sendmail = Sendmail.new(@config.smtp_host, @config.smtp_port,
+				@config.test)
+	begin
+	  sendmail.send(passmail)
+	  status[mail] = _('Success')
+	rescue
+	  status[mail] = _('Failed')
+	end
+      }
+
+      ar = []
+      status.each {|mail, result|
+	ar << [:li, mail, ' : ', result.to_s]
+      }
+      return c_notice(_('Send Password done')) {
+	[:ul, ar]
+      }
+    end
+
+    def sendpass_show_form
+      members = @site.member.list.sort
+      return c_notice(_('Send Password')) {
+	[[:h2, _('You can send password for the members.')],
+	  [:p, _('Please select members to send password.')],
+	  [:form, {:action=>'.sendpass', :method=>'post',
+	      :style=>'text-align: center; margin: 32px 0 48px;'},
+	    [:ul, members.map {|member|
+		[:li, [:input, {:type=>'checkbox', :name=>member}, ' '+member]]
+	      }
+	    ],
+	    [:input, {:type=>'submit', :value=>_('Send Password')}]]]
+      }
+    end
+  end
+end
+
+if $0 == __FILE__
+  require 'qwik/test-common'
+  require 'qwik/test-module-public'
+  $test = true
+end
+
+if defined?($test) && $test
+  class TestActSendPass < Test::Unit::TestCase
+    include TestSession
+
+    def test_all
+      t_add_user
+
+      res = session '/test/.sendpass'
+      ok_title 'Send Password'
+    end
+  end
+end
