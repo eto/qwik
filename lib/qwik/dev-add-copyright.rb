8386efbcaf0ddb83b0d7a5a591a166a01c67517f
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2006-07-19 04:56:11 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@274 4820488e-9c09-0410-87d0-9d66fe36c344

---
+++ lib/qwik/dev-add-copyright.rb
@@ -0,0 +1,23 @@
+# Copyright (C) 2003-2006 Kouichirou Eto, All rights reserved.
+# This is free software with ABSOLUTELY NO WARRANTY.
+# You can redistribute it and/or modify it under the terms of the GNU GPL 2.
+
+$LOAD_PATH << '..' unless $LOAD_PATH.include? '..'
+require 'qwik/util-pathname'
+
+COPYRIGHT_BANNER = <<END
+# Copyright (C) 2003-2006 Kouichirou Eto, All rights reserved.
+# This is free software with ABSOLUTELY NO WARRANTY.
+# You can redistribute it and/or modify it under the terms of the GNU GPL 2.
+
+END
+
+ARGV.each {|fname|
+  #s = File.read(fname)
+  s = fname.path.read
+  unless /\A#/ =~ s
+    s = COPYRIGHT_BANNER + s
+    fname.path.write(s)
+    #File.open(fname, "w"){|f| f.write(s) }
+  end
+}
