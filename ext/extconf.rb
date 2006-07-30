28d5ecec87565f53ee636895478e37e0563824a4
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2006-07-30 13:08:08 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@333 4820488e-9c09-0410-87d0-9d66fe36c344

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,2 @@
-# Copyright (C) 2003-2006 Kouichirou Eto, All rights reserved.
-# This is free software with ABSOLUTELY NO WARRANTY.
-# You can redistribute it and/or modify it under the terms of the GNU GPL 2.
-
 require 'mkmf'
 create_makefile('xmlformatter')
