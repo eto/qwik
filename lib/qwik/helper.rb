6468d1bd3382de312635f937f4d0dd4556451cff
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2007-08-11 15:22:14 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@420 4820488e-9c09-0410-87d0-9d66fe36c344

---
+++ lib/qwik/helper.rb
@@ -0,0 +1,6 @@
+# Copyright (C) 2003-2006 Kouichirou Eto, All rights reserved.
+# This is free software with ABSOLUTELY NO WARRANTY.
+# You can redistribute it and/or modify it under the terms of the GNU GPL 2.
+
+dir = File.expand_path(File.dirname(__FILE__) + '/../../lib')
+$LOAD_PATH.unshift(dir) if !$LOAD_PATH.include?(dir)
