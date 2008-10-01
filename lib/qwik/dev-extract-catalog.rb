cb9eb18346a73fa9883a7acf50722184aade1d81
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2008-10-01 14:47:21 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@491 4820488e-9c09-0410-87d0-9d66fe36c344

--- lib/qwik/dev-extract-catalog.rb
+++ lib/qwik/dev-extract-catalog.rb
@@ -1,4 +1,8 @@
 #! /usr/bin/ruby
+# Copyright (C) 2003-2008 AIST, All rights reserved.
+# This is free software with ABSOLUTELY NO WARRANTY.
+# You can redistribute it and/or modify it under the terms of the GNU GPL 2.
+
 # Description:
 # Read catalog-ja.rb and catarog-ml-ja.rb, output flat text file.
 #   argument: path to lib/qwik
@@ -13,17 +17,16 @@ if ARGV.length < 1
   exit(1)
 end
 
+def die(msg)
+  warn msg
+  exit(1)
+end
+
 libpath = Pathname.new(ARGV[0])
 catalog_ja = libpath + 'catalog-ja.rb'
 catalog_ml_ja = libpath + 'catalog-ml-ja.rb'
-unless catalog_ja.exist?
-  warn "#{catalog_ja} is not exist."
-  exit(1)
-end
-unless catalog_ml_ja.exist?
-  warn "#{catalog_ml_ja} is not exist."
-  exit(1)
-end
+die "#{catalog_ja} is not exist." unless catalog_ja.exist?
+die "#{catalog_ml_ja} is not exist." unless catalog_ml_ja.exist?
 
 class String
   def trim_line!
@@ -38,36 +41,48 @@ class String
     self.sub!(/\A['"]/, '')
     self.sub!(/['"]\Z/, '')
   end
+end
 
+def parse_file(input_path)
+  out = ''
+  input_path.open("r").each_line do |line|
+    line.trim_line!
+    case line
+    when /^#/, /^:charset/, /^:codeconv_method/
+      next
+    when /^(.+)\s*=>$/
+      k = $1
+      k.trim_line!
+      k.strip_metachar!
+      out << k+"\n"
+    when /^(.+)\s*=>\s+(.+)\s*,$/
+      k = $1
+      k.trim_line!
+      k.strip_metachar!
+      v = $2
+      v.trim_line!
+      v.strip_metachar!
+      out << k+"\n"
+      out << v+"\n"
+      out << "\n"
+    when /^(.+)\s*,$/
+      v = $1
+      v.trim_line!
+      v.strip_metachar!
+      out << v+"\n"
+      out << "\n"
+    end
+  end
+  return out
 end
 
 def process(output_path, input_path)
-  File.open(output_path, "w") do |output|
-    File.open(input_path, "r").each_line do |line|
-      line.trim_line!
-      case line
-      when /^#/, /^:charset/, /^:codeconv_method/
-	next
-      when /^(.+)\s*=>$/
-	k = $1
-	k.trim_line!
-	k.strip_metachar!
-	output.puts k
-      when /^(.+)\s*=>\s+(.+)\s*,$/
-	k = $1
-	k.trim_line!
-	k.strip_metachar!
-	v = $2
-	v.trim_line!
-	v.strip_metachar!
-	output.puts k, v, ''
-      when /^(.+)\s*,$/
-	v = $1
-	v.trim_line!
-	v.strip_metachar!
-	output.puts v, ''
-      end
-    end
+  input_path = Pathname.new input_path
+  out = parse_file(input_path)
+
+  output_path = Pathname.new output_path
+  output_path.open("w") do |output|
+    output.print out
   end
 end
 
