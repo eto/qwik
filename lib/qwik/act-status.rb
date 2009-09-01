1f193e427f51cf1c7101ecd819f6e6658f93b9a8
eto <eto@4820488e-9c09-0410-87d0-9d66fe36c344>
2009-09-01 12:00:07 +0000

    update.
    git-svn-id: svn://rubyforge.org/var/svn/qwik/qwik@582 4820488e-9c09-0410-87d0-9d66fe36c344

--- lib/qwik/act-status.rb
+++ lib/qwik/act-status.rb
@@ -29,15 +29,46 @@ module Qwik
 	vsz = vsz.to_i / 1000
 	rss = rss.to_i / 1000
 
-	str << "#{vsz}MB	#{rss}MB	#{command}"
+	str << "#{vsz}MB	#{rss}MB	#{command}\n"
       }
       str
     end
 
-    def act_status
+    def status_get_objects
+      ar = []
+      ObjectSpace.each_object {|obj|
+        ar << obj.class.name
+      }
+
+      hash = Hash.new(0)
+      ar.each {|classname|
+        hash[classname] += 1
+      }
+
+      ar2 = []
+      hash.each {|k, v|
+        ar2 << |k, v|
+      }
+
+      ar3 = ar2.sort
+#      ar3 = ar2.sort {|a, b|
+#        a[0] < b[0]
+#      }
+
       str = ''
-      str << status_get_date
+#      hash.each {|k, v|
+      ar3.each {|k, v|
+        str << "#{v}\t#{k}\n"
+      }
+      str
+    end
+
+
+    def act_status
+      str = "status\n"
+#      str << status_get_date
       str << status_get_memory
+      str << status_get_objects
       return c_notice(_('Status')) {
 	[[:h2, _('Status')],
 	  [:pre, str]]
