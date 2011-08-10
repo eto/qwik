e6665b17a20b5797ec9596fe3f2967b3d8d25206
SHIMADA Koji <snoozer.05@gmail.com>
2011-08-10 16:57:27 +0900

    fix SiteBackup#TAR_CMD not to specify path directly.

--- lib/qwik/act-sitebackup.rb
+++ lib/qwik/act-sitebackup.rb
@@ -102,9 +102,9 @@ If you change anything on the site, it\'ll recreate the archive.
   end
 
   class SiteBackup
-    TAR_CMD = '/bin/tar'	# path to tar command (GNU version is required)
+    TAR_CMD = `which tar`.chomp # path to tar command (GNU version is required)
     def self.command_exist?
-      TAR_CMD.path.executable?
+      !TAR_CMD.empty?
     end
 
     def initialize(site, config)
